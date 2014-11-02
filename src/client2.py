e01407a0feddcc18378e9a33d7527b9ee49b1758
James Munroe <jmunroe@mun.ca>
2014-11-01 23:47:12 -0230

    modify client to include GetImportTargetLogs

---
+++ src/client2.py
@@ -0,0 +1,57 @@
+import requests
+import time
+from lxml import etree
+
+# connect to server
+#host = 'localhost'
+#port = 8080
+
+# need to get this from VM
+server_ip = '192.168.2.18'
+server_port = 80
+
+# IP is localhost. Can make this more dynamic...
+client_ip = '192.168.2.17:8000'
+manifest = 'M2IAAS-256EBF9F-Cmanifest.xml'
+manifest = '1312-28801ECA87-Cmanifest.xml'
+
+def post(payload):
+    url = "http://%s:%d/" % (server_ip, server_port)
+
+    r = requests.post(url, data = payload)
+    try:
+        e = etree.fromstring(r.content)
+        print etree.tostring(e, pretty_print=True)
+    except:
+        print r.content
+
+done = False
+while True:
+    ## DescribeConversionTasks
+    payload = {'Action' : 'DescribeConversionTasks',}
+    post(payload)
+
+    # get status
+    payload = {'Action' : 'GetImportTargetStatus',}
+    post(payload)
+
+    # wait for 5 seconds
+    delay = 1
+    print "Waiting %d seconds..." % delay
+    print
+    time.sleep(delay)
+
+    # if import complete, break
+    if done:
+        break
+
+    # check status
+    done = True
+
+# get status
+payload = {'Action' : 'GetImportTargetStatus',}
+post(payload)
+
+# get log file
+payload = {'Action' : 'GetImportTargetLogs',}
+post(payload)
