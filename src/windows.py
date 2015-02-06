25f80723a1f05d72e563f9d5fab75875392f8fda
feoff <feoff3@gmail.com>
2015-02-06 17:40:51 +0300

    pass diskno via parm

--- src/windows.py
+++ src/windows.py
@@ -50,13 +50,13 @@ class Windows(object):
 
     def postprocess(self , device):
         #TOOD: add some specific parms
-        env = os.environ.copy()
+        
         # set the disk number - the last digit of device (works for 10 disks only)
-        env['DISK_N'] = device[-1]
+        diskn = device[-1]
         curdir = os.path.dirname(os.path.realpath(__file__))
-        if self.callBatch(curdir+'\\postprocess\\discover_new_drive.bat' , env) <> 0 or \
-           self.callBatch(curdir+'\\postprocess\\change_boot.bat' , env) <> 0 or \
-           self.callBatch(curdir+'\\postprocess\\set_ip.bat' , env) <> 0:
+        if self.callBatch(curdir+'\\postprocess\\discover_new_drive.bat ' + str(diskn)) <> 0 or \
+           self.callBatch(curdir+'\\postprocess\\change_boot.bat') <> 0 or \
+           self.callBatch(curdir+'\\postprocess\\set_ip.bat') <> 0:
             
             logger.error("Error postprocessing the instance image")
             raise Exception("Cannot postprocess the image")
