5a5ae88268c75f331a3a756a5f1c9d619877eb1c
feoff <feoff3@gmail.com>
2015-02-15 18:41:50 +0300

    cehck for 32 bit cmd

--- src/postprocess/change_boot.bat
+++ src/postprocess/change_boot.bat
@@ -1,3 +1,6 @@
+rem if we run from 32-bit cmd on 64-bit windows
+if exist C:\Windows\sysnative cd C:\Windows\sysnative
+
 bcdedit /copy {current} /d "Backup of default entry"
 bcdedit /set {current} device partition=X:
 bcdedit /set {current} osdevice partition=X:
