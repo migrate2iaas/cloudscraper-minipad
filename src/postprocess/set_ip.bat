3d224c8d1e363bb81b1f7da69c43332835216cf9
feoff <feoff3@gmail.com>
2015-03-16 22:44:18 +0300

    see #682, reset the default interface name

--- src/postprocess/set_ip.bat
+++ src/postprocess/set_ip.bat
@@ -1,2 +1,3 @@
 rem we get IP from this template and add it to the the adjust script
+netsh interface set interface name="Local Area Network" newname="VirtualNetworkAdapter"
 netsh -c interface dump > X:\CloudscraperBootAdjust\adjust\netsh_dump.txt
