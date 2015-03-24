deb5cbf1a9999a0443ee6cd11ba125846c4e2491
feoff <feoff3@gmail.com>
2015-03-24 12:16:08 +0300

    see #682, fixed typo

--- src/postprocess/set_ip.bat
+++ src/postprocess/set_ip.bat
@@ -1,3 +1,3 @@
 rem we get IP from this template and add it to the the adjust script
-netsh interface set interface name="Local Area Network" newname="VirtualNetworkAdapter"
+netsh interface set interface name="Local Area Connection" newname="VirtualNetworkAdapter"
 netsh -c interface dump > X:\CloudscraperBootAdjust\adjust\netsh_dump.txt
