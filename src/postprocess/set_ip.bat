78d7fab3a07f8256d5ad8ecc9e4d0c8ba3a02a4a
feoff <feoff3@gmail.com>
2015-04-20 21:42:45 +0300

    dumping the eth interface name

--- src/postprocess/set_ip.bat
+++ src/postprocess/set_ip.bat
@@ -1,3 +1,6 @@
 rem we get IP from this template and add it to the the adjust script
-netsh interface set interface name="Local Area Connection" newname="VirtualNetworkAdapter"
+set newAdapterName=VirtualNetworkAdapter
+
+netsh interface set interface name="Local Area Connection" newname="%newAdapterName%"
 netsh -c interface dump > X:\CloudscraperBootAdjust\adjust\netsh_dump.txt
+wmic NIC where "NetEnabled=true and NetConnectionID like '%newAdapterName%'" get Name /value > X:\CloudscraperBootAdjust\adjust\adapter_name.txt
