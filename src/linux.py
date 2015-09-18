00eb7785f41bebdbcbeafc66dec29989a73a68e3
feoff <feoff3@gmail.com>
2015-09-18 14:28:41 +0300

    updated logging

--- src/linux.py
+++ src/linux.py
@@ -12,7 +12,7 @@ __author__ = "Vladimir Fedorov"
 __copyright__ = "Copyright (C) 2013 Migrate2Iaas"
 #---------------------------------------------------------
 
-import logger
+import logging
 import traceback
 
 
