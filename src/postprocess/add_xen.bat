a3787c1c3a76c12fd643f42ac8058b254ed8fd3f
feoff <feoff3@gmail.com>
2015-12-20 18:44:25 +0300

    xen updated

--- src/postprocess/add_xen.bat
+++ src/postprocess/add_xen.bat
@@ -1,2 +1,5 @@
-"C:\Deployment Tools\amd64\DISM\dism.exe" /Image:X: /Add-Driver:X:\Xen\x86\xeniface.inf /ForceUnsigned
-"C:\Deployment Tools\amd64\DISM\dism.exe" /Image:X: /Add-Driver:X:\Xen\x86\xenvbd.inf /ForceUnsigned
+copy /Y C:\Xen\amd64 X:\Xen
+copy /Y C:\Xen X:\Xen
+
+"C:\Deployment Tools\amd64\DISM\dism.exe" /Image:X: /Add-Driver:X:\CloudscraperBootAdjust\Xen\xeniface.inf /ForceUnsigned
+"C:\Deployment Tools\amd64\DISM\dism.exe" /Image:X: /Add-Driver:X:\CloudscraperBootAdjust\Xen\xenvbd.inf /ForceUnsigned
