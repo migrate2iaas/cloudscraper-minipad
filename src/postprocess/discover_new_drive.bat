f176b9aeca7d9113fac0ba5c4654f922640e46f8
feoff <feoff3@gmail.com>
2016-06-22 02:53:46 +0300

    new wait routine implemented

--- src/postprocess/discover_new_drive.bat
+++ src/postprocess/discover_new_drive.bat
@@ -4,7 +4,9 @@ if "%DISK_N%"=="" set DISK_N=1
 echo rescan > rescan.txt
 diskpart /s rescan.txt
 rem wait till volume is recognized after the rescan (that is async in Windows)
-ping 1.1.1.1 -n 1 -w 320000 
+ping 1.1.1.1 -n 1 -w 180000 
+rem try to wait via waitfor
+waitfor /T 180 pause >nul
 
 echo select disk %DISK_N% >> rescan.txt
 echo select partition 1 >> rescan.txt
