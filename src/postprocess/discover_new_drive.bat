e6ee4d41342d5828397c7750e8eff62ca9c0d248
feoff <feoff3@gmail.com>
2016-06-22 02:48:22 +0300

    changed the interval value

--- src/postprocess/discover_new_drive.bat
+++ src/postprocess/discover_new_drive.bat
@@ -4,7 +4,7 @@ if "%DISK_N%"=="" set DISK_N=1
 echo rescan > rescan.txt
 diskpart /s rescan.txt
 rem wait till volume is recognized after the rescan (that is async in Windows)
-ping 1.1.1.1 -n 1 -w 160000 > nul
+ping 1.1.1.1 -n 1 -w 320000 
 
 echo select disk %DISK_N% >> rescan.txt
 echo select partition 1 >> rescan.txt
