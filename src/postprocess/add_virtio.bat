06fb49ba13e0273f696167251470699d22293d88
feoff <feoff3@gmail.com>
2016-08-03 16:50:13 +0300

    added boot of old ntldr systems

--- src/postprocess/add_virtio.bat
+++ src/postprocess/add_virtio.bat
@@ -1,1 +1,1 @@
-"C:\Deployment Tools\amd64\DISM\dism.exe" /Image:X: /Add-Driver:X:\CloudscraperBootAdjust\virtio\viostor.inf /ForceUnsigned
+if not exist X:\boot.ini "C:\Deployment Tools\amd64\DISM\dism.exe" /Image:X: /Add-Driver:X:\CloudscraperBootAdjust\virtio\viostor.inf /ForceUnsigned
--- src/postprocess/change_boot.bat
+++ src/postprocess/change_boot.bat
@@ -1,4 +1,6 @@
 rem if we run from 32-bit cmd on 64-bit windows
+if exist X:\boot.ini goto WIN2003
+
 if exist C:\Windows\sysnative\bcdedit.exe cd C:\Windows\sysnative
 bcdedit /copy {current} /d "Recovery OS"
 bcdedit /set {current} device partition=X:
@@ -6,4 +8,13 @@ bcdedit /set {current} osdevice partition=X:
 bcdedit /set {current} bootlog yes
 bcdedit /set {current} bootstatuspolicy ignoreallfailures
 bcdedit /set {current} description MigratedOS
+goto END
 
+:WIN2003
+
+bcdedit /create {NTLDR} /d "Migrated legacy OS"
+bcdedit /set {NTLDR} device partition=X:
+bcdedit /set {NTLDR} path \NTLDR
+bcdedit /default {NTLDR}
+
+:END
